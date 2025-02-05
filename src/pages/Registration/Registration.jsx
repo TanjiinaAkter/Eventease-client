7647012de9ca2016f3431a20e80dbcae831938ae
tanjiina <tanjina1042@gmail.com>
2025-02-05 23:03:02 +0600

    login  page done

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,103 +1,87 @@
-import { IoHome } from "react-icons/io5";
 import { Link } from "react-router-dom";
 
 const Registration = () => {
   return (
-    <div>
-      <div
-        style={{
-          backgroundImage:
-            "url('https://i.ibb.co.com/cS96Ft6R/table-5696243-1280.jpg')",
-        }}
-        className="flex relative  bg-no-repeat bg-center bg-cover  justify-center items-center w-full h-screen">
-        <div className="bg-[#0a1316] h-screen opacity-80 inset-0 absolute"></div>
+    <div className="relative bg-black w-full h-full min-h-screen p-6">
+      <div className="bg-no-repeat bg-cover object-cover bg-center w-full h-full absolute top-0 left-0 bg-[url('https://i.ibb.co/cS96Ft6R/table-5696243-1280.jpg')]"></div>
+
+      <div className="absolute bg-black  opacity-50 inset-0 "></div>
+
+      <div className="flex justify-start items-center p-7">
         <Link to="/">
-          <h3 className="absolute hover:scale-105 hover:border-transparent top-10 border-2 p-2 duration-700 hover:bg-white transition-all ease-in-out  hover:text-black border-white text-white font-semibold left-10 flex items-center gap-2">
-            <IoHome className="text-[#44cfbf] text-2xl " />
-            BACK TO HOME
-          </h3>
+          <button>BACK TO HOME</button>
         </Link>
-        <div className="absolute mt-[350px] md:mt-0 form-container border transition-all border-[#44cfbf] duration-700 ease-in-out hover:shadow-[#44cfbf] shadow-lg  bg-[#275e5eb8] rounded-lg w-[90%] md:w-[50%] lg:w-[40%] py-8 px-6 ">
-          <h2 className="text-white text-3xl md:text-4xl text-center font-semibold ">
+      </div>
+      <div className="relative z-10 flex justify-center items-center border border-y-amber-300 ">
+        <div className=" shadow-2xl p-6 bg-[#0f1c1c] rounded-2xl w-full md:w-[40%] h-full">
+          <h3 className="text-white text-center text-2xl md:text-4xl font-semibold uppercase">
             Create your account
-          </h2>
-          <p className="text-white uppercase md:text-sm font-semibold mt-3 text-center  ">
-            Access your account with a quick login!
+          </h3>
+          <p className="text-white  text-center text-sm my-2 font-semibold uppercase">
+            Register now and enjoy a seamless experience!
           </p>
-          <hr className="w-[10%] mx-auto h-[5px] bg-[#44cfbf] my-3" />
-          <form className="form ">
-            <div className="form-group flex flex-col space-y-3">
-              <label
-                className="text-lg font-semibold  text-white"
-                htmlFor="name">
+          <hr className="w-[10%] mx-auto h-[5px] bg-[#44cfbf] my-3 " />
+          <div className="form ">
+            <div className=" flex flex-col">
+              <label className="text-white text-lg" htmlFor="name">
                 Your Name
               </label>
               <input
-                className="px-3 text-white py-2 border-b-[1px] border-b-amber-50   focus:border-b-[3px] rounded-none border-transparent  focus:outline-none"
                 type="text"
-                id="name"
-                name="name"
-                placeholder="Your Name"
-                required
+                name=""
+                id=""
+                placeholder="Enter your name"
+                className="py-3 w-full border-b-[0.5px] mb-2 border-b-amber-50 rounded-none border-transparent text-white focus:border-b-white focus:border-b-[3px]  focus:outline-0"
               />
             </div>
-            <div className="form-group flex flex-col space-y-2">
-              <label
-                className="text-lg font-semibold  text-white"
-                htmlFor="email">
+            <div className="flex flex-col space-y-3">
+              <label className="text-white text-lg" htmlFor="email">
                 Your Email
               </label>
               <input
-                className="px-3 text-white py-2 border-b-[1px] border-b-amber-50   focus:border-b-[3px] rounded-none border-transparent  focus:outline-none"
                 type="email"
-                id="email"
-                name="email"
-                placeholder="Your password"
-                required
+                name=""
+                id=""
+                placeholder="Enter your email"
+                className="py-3 text-white border-b-white focus:border-b-[3px] border-transparent focus:outline-none border-b-[1px] rounded-none"
               />
             </div>
-            <div className="form-group flex flex-col space-y-3">
-              <label
-                className="text-lg font-semibold text-white "
-                htmlFor="email">
+            <div className="flex flex-col space-y-3">
+              <label className="text-white text-lg mt-3" htmlFor="password">
                 Your Password
               </label>
               <input
-                className="px-3 text-white py-2 border-b-[1px] border-b-amber-50   focus:border-b-[3px] rounded-none border-transparent  focus:outline-none"
                 type="password"
-                id="password"
-                name="password"
+                name=""
+                id=""
                 placeholder="Enter your password"
-                required
+                className="py-3 border-transparent border-b-white border-b-[1px] focus:border-b-[3px] text-white focus:outline-none"
               />
             </div>
-            <div className="form-group flex flex-col space-y-3">
-              <label
-                className="text-lg font-semibold  text-white"
-                htmlFor="photo">
+            <div className="flex flex-col space-y-3">
+              <label className="text-white text-lg mt-3" htmlFor="photo">
                 Your Photo
               </label>
               <input
-                className="px-3 text-white py-2 border-b-[1px] border-b-amber-50   focus:border-b-[3px] rounded-none border-transparent  focus:outline-none"
                 type="photo"
-                id="photo"
-                name="photo"
-                placeholder="Your Photo"
-                required
+                name=""
+                id=""
+                placeholder="Enter your photo"
+                className="py-3 border-transparent border-b-white border-b-[1px] focus:border-b-[3px] text-white focus:outline-none"
               />
             </div>
-            <button className="my-7 w-full" type="submit">
-              Submit
-            </button>
-            <Link to="/login">
-              <p className="text-white cursor-pointer">
-                Already have an account?
-                <span className="text-[#d5944a] pl-1 text-lg hover:border-b-2">
-                  Login
-                </span>
-              </p>
-            </Link>
-          </form>
+            <div className="w-full grid my-7">
+              <button>Register</button>
+            </div>
+
+            <p className="text-white">
+              {" "}
+              Already have an account?{" "}
+              <span className="text-[#d5944a] text-lg hover:border-b-2">
+                Login Now
+              </span>{" "}
+            </p>
+          </div>
         </div>
       </div>
     </div>
